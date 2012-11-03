73f5cbeea72e8b94e194a4aff39a68de74f1d54c
Ubuntu <ubuntu@littlebadwolf.com>
2012-11-03 21:51:41 +0000

    Pulled helper names into the routes.

--- app/controllers/route66_controller.rb
+++ app/controllers/route66_controller.rb
@@ -1,7 +1,26 @@
 class Route66Controller < ApplicationController
 
+	before_filter :load_routes
+
 	def index
-		@routes = Rails.application.routes.routes.routes
+	end
+	
+	def load_routes
+    @routes = []
+    Rails.application.routes.routes.routes.each do |r|
+      verb = r.verb.to_s.scan( /\^(\w+)\$/).first
+      verb = verb ? verb.first : ''
+      new_route = {:precedence => r.precedence,
+                   :verb       => verb,
+                   :controller => r.defaults[:controller],
+                   :action     => r.defaults[:action],
+                   :path       => r.path.spec,
+                   :name       => '' }
+      @routes[r.precedence] = new_route
+		end
+		Rails.application.routes.routes.named_routes.each do |name, route|
+			@routes[route.precedence][:name] = name
+		end
 	end
 
 end
