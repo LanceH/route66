bd21ad477fb6a0a1fe3935e5927149e5fb27a580
Lance Hampton <lance.hampton@gmail.com>
2012-11-04 00:39:02 +0000

    fixed links on controllers containing /'s

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 	match "route66" => "route66#index"
-	match "route66/controller/:id" => "route66#show", :as => :route66_controller, :id => /.+/
+	match "route66/controller/:id" => "route66#show", :as => :route66_controller
 end
